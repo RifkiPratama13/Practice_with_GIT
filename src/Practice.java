7dd86b942a7932fb0dcf87ef8853d328b45a75af
Rifki Pratama <yudarifki19@gmail.com>
2022-02-22 18:14:44 +0700

    create branch b

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,6 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Branch A");
+        System.out.println("Branch B");
         System.out.println("Feature 1");
     }
 }
