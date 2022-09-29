afbc8390306361c09e0967c7e5aeb88c561fc7b0
Head50 <yudarifki19@gmail.com>
2022-09-29 13:33:15 +0700

    test git

--- src/Practice.java
+++ src/Practice.java
@@ -2,5 +2,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Branch A");
         System.out.println("Feature 1");
+        System.out.println("Try update");
     }
 }
